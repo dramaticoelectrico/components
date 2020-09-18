2b655e171fc641a560eaac76adbabd59d6e0c85b
james mcmenemy <cltc1888@yahoo.com>
2020-09-17 20:27:43 -0500

    components: add api js files

--- public/js/combobox.js
+++ public/js/combobox.js
@@ -1,14 +1,18 @@
-;(function () {
+;(function (config) {
+  /**
+   * config
+   * data: object
+   * form: form id
+   * list: list parent id
+   * input: input id
+   * styles: {highligt: color}
+   */
+
   const listParent = document.getElementById('owned_listbox')
   const form = document.getElementById('combobox-form')
   const input = document.getElementById('search')
   let listIndex = -1
 
-  const getData = async (val) => {
-    const request = await fetch('http://127.0.0.1:5500/api/dogs.json?=' + val)
-    const response = await request.json()
-    return response.dogs
-  }
   const resetListBox = () => {
     listParent.innerHTML = ''
     form.setAttribute('aria-expanded', false)
@@ -18,13 +22,14 @@
   function filterData(str, data) {
     if (!data.length) return resetListBox()
     const regxp = new RegExp(`^${str}`, 'gi')
-    return data.filter((item) => item.match(regxp))
+    const filtered = data.filter((item) => item.match(regxp))
+    buildList(filtered)
   }
   function buildList(dataList) {
     let list
     listParent.innerHTML = ''
     listIndex = -1
-    if (!dataList.length) return resetListBox()
+    if (!dataList.length || input.value === '') return resetListBox()
     form.setAttribute('aria-expanded', true)
 
     for (var i = 0; i < dataList.length; i++) {
@@ -36,15 +41,7 @@
       listParent.append(list)
     }
   }
-  async function createDataList(e) {
-    const query = e.target.value
-    if (!query) return resetListBox()
 
-    const response = await getData(query)
-    const filterd = filterData(query, response)
-
-    buildList(filterd)
-  }
   function traverseList(listIndex) {
     listParent.querySelectorAll('li').forEach((item) => {
       item.removeAttribute('style')
@@ -88,10 +85,10 @@
         event.preventDefault()
         if (!els[listIndex]) return
         input.value = els[listIndex].textContent
-        listParent.innerHTML = ''
+        resetListBox()
         break
       default:
-        console.log('looks like an error or missed')
+        console.warn('a key event ran through ', event.code)
     }
   }
 
@@ -108,9 +105,37 @@
     }
   }
   /**
-   * Events
+   * getData()
+   * @param {String} val
    */
+  const getData = async (val) => {
+    try {
+      const request = await fetch('http://127.0.0.1:5500/api/dogs.json?=' + val)
+      return await request.json()
+    } catch (error) {
+      console.error({ error, method: 'getData()' })
+    }
+  }
+  /**
+   * createDataList()
+   * Outside function to use with API
+   * @param {Object} e
+   */
+  async function createDataList(e) {
+    const query = e.target.value
+    const response = await getData(query)
+
+    // call the api method here
+    filterData(query, response.dogs)
+
+    // buildList(filterd)
+  }
   input.addEventListener('input', createDataList)
+
+  /**
+   * API Events
+   */
+
   form.addEventListener('submit', formSubmit)
   input.addEventListener('keydown', keyCodeEvents)
   listParent.addEventListener('click', handleListClick)
