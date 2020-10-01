e8155901c16536b5d36c5e8664ce3578bd84e4f0
james mcmenemy <cltc1888@yahoo.com>
2020-09-30 20:35:57 -0500

    components: add the dialog

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,14 +1,20 @@
+const FOCUSABLE_ELS = [
+  'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]',
+]
 const Modal = function (config) {
   const { id, trigger } = config
   const modal = document.getElementById(id)
   const modalBtn = document.getElementById(trigger)
   const closeBtn = modal.querySelectorAll('.dialog-close')
   const agreeBtn = modal.querySelector('.dialog-agree')
+  const focusable = [...modal.querySelectorAll(FOCUSABLE_ELS)]
 
   function launchDialog(e) {
     e.preventDefault()
     modal.dataset.hidden = false
     modal.setAttribute('aria-hidden', false)
+    modal.setAttribute('tabIndex', '1')
+    modal.focus()
 
     modal.addEventListener('transitionend', removeModalTransitions)
 
@@ -28,13 +34,39 @@ const Modal = function (config) {
 
     modal.classList.remove('active')
     modal.setAttribute('aria-hidden', true)
+    modal.setAttribute('tabIndex', '-1')
 
     closeBtn.forEach((btn) => btn.removeEventListener('click', closeDialog))
     agreeBtn.removeEventListener('click', closeDialog)
   }
 
+  function keyEvents(event) {
+    const type = event.shiftKey && event.keyCode === 9 ? 'shiftTab' : event.code
+    switch (type) {
+      case 'Escape':
+        closeDialog(event)
+        break
+      case 'Tab':
+        if (document.activeElement === focusable[focusable.length - 1]) {
+          focusable[0].focus()
+          event.preventDefault()
+        }
+        break
+      case 'shiftTab':
+        if (document.activeElement === focusable[0]) {
+          focusable[focusable.length - 1].focus()
+          event.preventDefault()
+        }
+        break
+      default:
+        console.log(type)
+        return
+    }
+  }
+
   function init() {
     modalBtn.addEventListener('click', launchDialog)
+    modal.addEventListener('keydown', keyEvents)
   }
 
   return { init: init }
