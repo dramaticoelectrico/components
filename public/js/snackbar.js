ac57a71971883ce42d3439e88f2ad2e50f12fcb1
james mcmenemy <cltc1888@yahoo.com>
2020-11-18 18:27:40 -0600

    components: add forms elements

--- public/js/snackbar.js
+++ public/js/snackbar.js
@@ -1,13 +1,36 @@
-const SnackBar = function ({ el, status }) {
-  const snackbar = document.getElementById(el)
-  const dimensions = snackbar.getBoundingClientRect()
+/**
+ * UI status alert
+ * el: Target element
+ * status: error, warning, success
+ * auto: Boolean control show/hide. true by defult
+ * @param { el, status, auto } Object
+ * @returns Object init
+ */
+const SnackBar = function ({ el, status, auto }) {
+  const defaults = { el, status: 'success', auto: true }
+  const options = { ...defaults, el, status, auto }
+  const snackbar = document.getElementById(options.el)
+
+  function handlerCleanup(event) {
+    if (event.animationName === 'animationSnackbarClose') {
+      console.log('transition end', event)
+      snackbar.classList.remove('snackbar-active--close')
+      snackbar.removeAttribute('style')
+      snackbar.removeEventListener('animationend', handlerCleanup)
+    }
+  }
+
   const init = {}
+  init.show = function () {}
   init.open = function () {
-    snackbar.classList.add('active')
+    snackbar.setAttribute('style', 'display: block')
+    setTimeout(() => snackbar.classList.add('snackbar-active--open'), 100)
     snackbar.setAttribute('aria-hidden', false)
+    snackbar.addEventListener('animationend', handlerCleanup)
   }
   init.close = function () {
-    snackbar.classList.remove('active')
+    snackbar.classList.add('snackbar-active--close')
+    snackbar.classList.remove('snackbar-active--open')
     snackbar.setAttribute('aria-hidden', true)
   }
   return init
